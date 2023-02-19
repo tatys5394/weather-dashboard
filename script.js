260e6adae09e31c1a0b41dd6bf1f57fce318710b
tatys5394 <123910162+tatys5394@users.noreply.github.com>
2023-02-19 01:43:51 -0500

    added first layout in index

---
+++ script.js
@@ -0,0 +1,10 @@
+var APIKey = "104a3cf9c97eb36dae06eabd5aa7ae2d";
+
+// create user input box for cities
+// create an array of cities? or how do we get coordinates 
+// create localstorage to store user input. Specify state and country variables in your API call, as multiple countries or states might have cities with the same name. For the purposes of this guide, you can use the city variable that you just created.
+
+
+var city; 
+var queryURL = https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=104a3cf9c97eb36dae06eabd5aa7ae2d
+fetch(queryURL)
